fbd8f40b9599e4603490fb7f9bc65cd44fd74f3e
Benjamin Kollek <benkollek@gmail.com>
2016-06-24 13:25:59 -0400

    GUI work, added notes file

--- support.js
+++ support.js
@@ -5,8 +5,10 @@ function loadJS() {
 function selectMarkers(){
   if(document.getElementById("selectMarkersButton").innerHTML == "Done"){
     document.getElementById("selectMarkersButton").innerHTML = "Select Markers";
+    document.getElementById("selectMarkersButton").style.color = "black";
   }
   else {
     document.getElementById("selectMarkersButton").innerHTML = "Done";
+    document.getElementById("selectMarkersButton").style.color = "red";
   }
 };
