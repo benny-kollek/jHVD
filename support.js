ea4d90b445fbef40613474349f4d43160acc69f8
Benjamin Kollek <benkollek@gmail.com>
2016-06-24 14:51:10 -0400

    added mask

--- support.js
+++ support.js
@@ -1,8 +1,5 @@
-function loadJS() {
-  //document.getElementById("selectMarkersButton").addEventListener("click", selectMarkers());
-};
-
 function selectMarkers(){
+  loadImageMask();
   if(document.getElementById("selectMarkersButton").innerHTML == "Done"){
     document.getElementById("selectMarkersButton").innerHTML = "Select Markers";
     document.getElementById("selectMarkersButton").style.color = "black";
@@ -14,9 +11,21 @@ function selectMarkers(){
 };
 
 
-function load(){
+function loadImage(){
   var subjectN = document.getElementById("subjectNumber").value;
   document.getElementById("image").src = "lib/stills/v" + subjectN + "/001.png";
+};
+
+function loadImageMask() {
+  var maskSpace = document.getElementById("imageMaskSpace");
+  var image = document.getElementById("image");
+  var h = document.getElementById("image").height.toString() + "px";
+  var w = document.getElementById("image").width.toString() + "px";
+  console.log(h);
+  console.log(w);
+
+  maskSpace.style.height = h;
+  maskSpace.style.width = w;
 
 
-}
+};
